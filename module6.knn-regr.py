6d933971de77fbeb4020de3eea9687563ecedf73
DONGYOUNG KIM <68176212+dkim4741@users.noreply.github.com>
2025-11-16 23:41:36 -0800

    Update module6.knn-regr.py

--- module6.knn-regr.py
+++ module6.knn-regr.py
@@ -3,21 +3,23 @@ import numpy as np
 class KNNRegressor:
     def __init__(self, k):
         self.k = k
-        self.X = np.array([])
-        self.Y = np.array([])
+        self.X_list = []
+        self.Y_list = []
 
     def add_point(self, x, y):
-        self.X = np.append(self.X, float(x))
-        self.Y = np.append(self.Y, float(y))
+        self.X_list.append(float(x))
+        self.Y_list.append(float(y))
+
+    def finalize(self):
+        self.X = np.array(self.X_list)
+        self.Y = np.array(self.Y_list)
 
     def predict(self, query):
         if self.k > len(self.X):
             return "Error: integer k must be <= N."
 
         distances = np.abs(self.X - float(query))
-
         k_idx = np.argsort(distances)[:self.k]
-
         return np.mean(self.Y[k_idx])
 
 
@@ -33,11 +35,15 @@ def main():
         y = float(input(f"Point #{i+1} - y: "))
         model.add_point(x, y)
 
+    model.finalize()
+
     query = float(input("Enter X to predict Y: "))
 
     result = model.predict(query)
 
     print(f"For X = {query}, Predicted Y = {result}")
 
+
 if __name__ == "__main__":
     main()
+
